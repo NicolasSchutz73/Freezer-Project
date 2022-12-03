7bc1eadadd095e7973cea42ba18d4bec58aff30c
ArtemichouFowl <104560355+ArtemichouFowl@users.noreply.github.com>
2022-12-03 16:49:52 +0100

    Bouton like incroyable

--- crud/removeMusicLiked.php
+++ crud/removeMusicLiked.php
@@ -20,11 +20,26 @@ while ($row = mysqli_fetch_assoc($result)) {
     $emparray[] = $row['musiques'];
 }
 
+$musiques = explode(",", $emparray[0]);
+
 # Supprimer musique likée dans base de donnée
 
+<<<<<<< Updated upstream
 $newTab = str_replace($idMusicLiked.",", '',$emparray[0]);
 $sql="UPDATE `utilisateurs` SET `musiques` = '$newTab' WHERE `utilisateurs`.`id` = $idSession";
 $result = mysqli_query($mysqli, $sql);
 $sql="UPDATE `utilisateurs` SET `musiques` = '$newTab' WHERE `id` = '4' ";
+=======
+unset($musiques[array_search($idMusicLiked, $musiques)]);
+$newtab =implode(",",$musiques);
+
+$sql="UPDATE `utilisateurs` SET `musiques` = '$newtab' WHERE `utilisateurs`.`id` = $idSession";
+$result = mysqli_query($mysqli, $sql);
+$sql="UPDATE `playlists` SET `musiques` = '$newtab' WHERE `id` = '4' ";
+$result = mysqli_query($mysqli, $sql);
+
+#Fermeture connection
+mysqli_close($mysqli);
+>>>>>>> Stashed changes
 
 
