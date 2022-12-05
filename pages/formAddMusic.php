940fca246d7c9ee0f6a292af5091bfde0aeb5e2a
Maxime272003 <62075092+Maxime272003@users.noreply.github.com>
2022-12-05 13:08:12 +0100

    ajout musique

---
+++ pages/formAddMusic.php
@@ -0,0 +1,10 @@
+<h1>Ajout musique</h1>
+<div>
+    <input type="text" name="music" id="music" placeholder="Nom de la musique" />
+    <input type="text" name="artist" id="artist" placeholder="Nom de l'artiste" />
+    <input type="text" name="genre" id="genre" placeholder="Genre" />
+    <input type="file" accept=".png .jpeg" name="image" id="image" />
+    <input type="file" accept=".mp3" name="music" id="music" />
+</div>
+
+<button>Ajouter</button>
--- script/formulairesscript.js
+++ script/formulairesscript.js
@@ -1,13 +1,13 @@
 let main = document.querySelector("main")
-let formBlur = create("div",main,null,"form-blur")
-let formulaireContainer = create("div",formBlur,null,"formulaire-container")
+let formBlur = create("div", main, null, "form-blur")
+let formulaireContainer = create("div", formBlur, null, "formulaire-container")
 
 //croix
-let crossContain = create("div",formulaireContainer,null,"cross-form-contain")
-let crossForm = create("p",crossContain,"✖","cross")
-crossForm.addEventListener("click",closeForm)
+let crossContain = create("div", formulaireContainer, null, "cross-form-contain")
+let crossForm = create("p", crossContain, "✖", "cross")
+crossForm.addEventListener("click", closeForm)
 //Conteneur
-let formulaire = create("div",formulaireContainer,null,"formulaire")
+let formulaire = create("div", formulaireContainer, null, "formulaire")
 
 //Titre
 /*
@@ -24,99 +24,129 @@ mdp.type = "password";
 */
 
 //Popup
-let popupContainer = create("div",main,null,"popup-container")
+let popupContainer = create("div", main, null, "popup-container")
 popupContainer.classList.add("hidden")
-let popup = create("div",popupContainer,null,"popup")
+let popup = create("div", popupContainer, null, "popup")
 
 //Croix
-let crossPopup = create("p",popup,"✖","cross")
-crossPopup.addEventListener("click",closePopup)
+let crossPopup = create("p", popup, "✖", "cross")
+crossPopup.addEventListener("click", closePopup)
 
 //Message
-let popupMessage = create("p",popup,"Texte popup bla bla bla")
+let popupMessage = create("p", popup, "Texte popup bla bla bla")
 
 //Fermer Popup
-function closePopup(){
+function closePopup() {
     popupContainer.classList.toggle("visible")
     popupContainer.classList.toggle("hidden")
 }
 
 //ouvrir Popup
-function openPopup(message){
+function openPopup(message) {
     popupMessage.innerHTML = message;
     popupContainer.classList.toggle("hidden")
     popupContainer.classList.toggle("visible")
 }
 
 //ouvrir formulaire
-function openForm(){
+function openForm() {
     formBlur.style.display = "flex";
 }
 
 //fermer formulaire
-function closeForm(){
+function closeForm() {
     removeAllChild(formulaire)
-    formBlur.style.display="none"
+    formBlur.style.display = "none"
 }
 
 
 //Ajouter musique vers playliste
-function addMusicToPlaylist(idMusic){
+function addMusicToPlaylist(idMusic) {
     //recup formulaire via id musique
-    axios.get("pages/formAddToPlay.php?idmusic="+idMusic)
-    .then(function (response) {
-        //affichage page formulaire
-        formulaire.innerHTML = response.data;
-        //ouverture formulaire
-        openForm()
-
-        //affichage datas musique
-        axios.get("crud/getmusiquesplaylist.php?id="+idMusic+"&search=null")
+    axios.get("pages/formAddToPlay.php?idmusic=" + idMusic)
         .then(function (response) {
-            //change l'image de la musique du form
-            let imageMusique = document.querySelector(".formulaire img")
-            imageMusique.src = "images/musique/"+response.data[0].image
+            //affichage page formulaire
+            formulaire.innerHTML = response.data;
+            //ouverture formulaire
+            openForm()
 
-            //change le titre de la musique du formulaire
-            let nomMusique = document.querySelector(".formulaire span")
-            nomMusique.innerHTML = response.data[0].nom_music
-        })
+            //affichage datas musique
+            axios.get("crud/getmusiquesplaylist.php?id=" + idMusic + "&search=null")
+                .then(function (response) {
+                    //change l'image de la musique du form
+                    let imageMusique = document.querySelector(".formulaire img")
+                    imageMusique.src = "images/musique/" + response.data[0].image
 
-        //select parent options
-        let select = document.querySelector("select")
+                    //change le titre de la musique du formulaire
+                    let nomMusique = document.querySelector(".formulaire span")
+                    nomMusique.innerHTML = response.data[0].nom_music
+                })
 
-        //recup playlists
-        axios.get("crud/getallplaylists.php?search=null")
-        .then(function (response) {
-            for(playlist of response.data){
-                //options du formulaire
-                let option = create("option",select,playlist.nom)
-                option.value = playlist.id
-            }
-        })
+            //select parent options
+            let select = document.querySelector("select")
 
-        //Boutton de confirmation
-        let buttonForm = document.querySelector(".formulaire button")
-        buttonForm.addEventListener("click",function(){
-
-            //ajout
-            if (select.value!="null") {
-                
-                axios.get("crud/addmusicpl.php?idpl=" + select.value + "&idmusic=" + idMusic
-                ).then(function (response) {
-                    if(response.statusText!="OK"){
-                        openPopup("Une erreur est survenue")
-                    } else {
-                        openPopup("La musique a été ajoutée à la playlist !")
+            //recup playlists
+            axios.get("crud/getallplaylists.php?search=null")
+                .then(function (response) {
+                    for (playlist of response.data) {
+                        //options du formulaire
+                        let option = create("option", select, playlist.nom)
+                        option.value = playlist.id
                     }
                 })
-                closeForm()
 
-            } else {
-                let errorMessageContainer = document.querySelector(".formulaire div")
-                create("p",errorMessageContainer,"Vous devez choisir une playlist !")
-            }
+            //Boutton de confirmation
+            let buttonForm = document.querySelector(".formulaire button")
+            buttonForm.addEventListener("click", function () {
 
+                //ajout
+                if (select.value != "null") {
+
+                    axios.get("crud/addmusicpl.php?idpl=" + select.value + "&idmusic=" + idMusic
+                    ).then(function (response) {
+                        if (response.statusText != "OK") {
+                            openPopup("Une erreur est survenue")
+                        } else {
+                            openPopup("La musique a été ajoutée à la playlist !")
+                        }
+                    })
+                    closeForm()
+
+                } else {
+                    let errorMessageContainer = document.querySelector(".formulaire div")
+                    create("p", errorMessageContainer, "Vous devez choisir une playlist !")
+                }
+
+            })
         })
+}
+
+/* FORMULAIRE D'AJOUT DE MUSIQUE */
+
+function addSuggestion(suggestionID) {
+    //récupération du formulaire via id suggestion
+    axios.get("pages/formAddMusic.php?idSuggestion=" + suggestionID)
+        .then(function (response) {
+            //affichage page formulaire
+            formulaire.innerHTML = response.data;
+            //ouverture formulaire
+            openForm()
+        })
+
+    //Bouton de confirmation
+    let buttonForm = document.querySelector(".formulaire button")
+    buttonForm.addEventListener("click", function () {
+
+        //ajout
+        axios.get("crud/addMusicDB.php")
+            .then(function (response) {
+                if (response.statusText != "OK") {
+                    openPopup("Une erreur est survenue")
+                } else {
+                    openPopup("La musique a été ajoutée à la base de donnée !")
+                }
+            })
+        closeForm()
+
     })
 }
