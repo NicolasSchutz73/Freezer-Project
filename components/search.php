95500387234a409d1289520a8d95a36441608ab1
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2022-12-09 14:38:32 +0100

    modifications fichier search et le style

--- components/search.php
+++ components/search.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" type="text/css" href="<?php echo HOST_CSS . '/styleSearch.css'; ?>">
-   <script src="https://kit.fontawesome.com/bb4997e1c6.js" crossorigin="anonymous"></script>
-   <title>search bar</title>
-</head>
-
-<body>
    <div class="wrapSearch">
       <div class="search">
          <input type="text" class="searchTerm" placeholder="Recherche une musique, une playlist...">
@@ -20,7 +7,4 @@
       </div>
    </div>
 
-</body>
 <script src="<?php echo HOST_JS . '/scriptSearch.js' ?>"></script>
-
-</html>
